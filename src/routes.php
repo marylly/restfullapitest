9cfa65ba852f0f75bce7da35f8f39abde5f220ae
Marylly Araújo Silva <marylly@hacklab.com.br>
2017-07-23 16:25:22 -0300

    Added code coverage html report.

--- src/routes.php
+++ src/routes.php
@@ -1,45 +1,60 @@
 <?php
 use App\Controllers as Controllers;
 
-$app->get('/users[/]', function ($request, $response, $args) {
-    $users = new Controllers\User($this);
-    $todos = $users->AllUsers();
-    return $this->response->withJson($todos);
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->get('/user/{id}[/]', function ($request, $response, $args) {
-    $user = new Controllers\User($this);
-    $return = $user->getUser($args['id']);
-    return $response->withJson($return);
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
 });
 
-$app->post('/user[/]', function ($request, $response, $args) {
-    $content = $request->getParsedBody();
-    $user = new Controllers\User($this);
-    if($user->addNew($content)) {
-        return $response->getBody()->write($request->getBody());
-    } else {
-        return false;
-    }
-});
+// $app->get('/users[/]', function ($request, $response, $args) {
+//     $users = new Controllers\User($this);
+//     $todos = $users->AllUsers();
+//     return $this->response->withJson($todos);
+// });
 
-$app->put('/user/{id}[/]', function ($request, $response, $args) {
-    $id = $args['id'];
-    $content = $request->getParsedBody();
-    $user = new Controllers\User($this);
-    if($user->updUser($id,$content)) {
-        return "{ 'id' : " . $id . "}";
-    } else {
-        return false;
-    }
-});
+// $app->get('/user/{id}[/]', function ($request, $response, $args) {
+//     $user = new Controllers\User($this);
+//     $return = $user->getUser($args['id']);
+//     return $response->withJson($return);
+// });
 
-$app->delete('/user/{id}[/]', function ($request, $response, $args) {
-    $id = $args['id'];
-    $user = new Controllers\User($this);
-    if($user->delUser($id)) {
-        return "{ 'id' : " . $id . "}";
-    } else {
-        return false;
-    }
-});
+// $app->post('/user[/]', function ($request, $response, $args) {
+//     $content = $request->getParsedBody();
+//     $user = new Controllers\User($this);
+//     if($user->addNew($content)) {
+//         return $response->getBody()->write($request->getBody());
+//     } else {
+//         return false;
+//     }
+// });
+
+// $app->put('/user/{id}[/]', function ($request, $response, $args) {
+//     $id = $args['id'];
+//     $content = $request->getParsedBody();
+//     $user = new Controllers\User($this);
+//     if($user->updUser($id,$content)) {
+//         return "{ 'id' : " . $id . "}";
+//     } else {
+//         return false;
+//     }
+// });
+
+// $app->delete('/user/{id}[/]', function ($request, $response, $args) {
+//     $id = $args['id'];
+//     $user = new Controllers\User($this);
+//     if($user->delUser($id)) {
+//         return "{ 'id' : " . $id . "}";
+//     } else {
+//         return false;
+//     }
+// });
